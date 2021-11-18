fc635a69e7560a50ed2e4f8acc43d930e552f05a
saitako <saitako@outlook.com>
2021-11-18 20:01:32 +0800

    add/script

--- main.py
+++ main.py
@@ -1,44 +1,109 @@
+#! /usr/bin/python3
+
 import sys
 import subprocess
 import time
 import os
 from glob import glob
+from pathlib import Path
+from typing import Union
+
+multithread_test_path = Path('./multi_threaded/test.py')
+multithread_server_path = Path('./multi_threaded/server.py')
+multicast_test_path = Path('./multicast/multicast.py')
+
+GREETINGS = '''Greetings!
+Options:
+    "mt": Multi-threaded Server
+        "t": test only
+        "s": server only
+    "mc": Multicast Server
+        "t": test only
+    "r": RPC and RMI
+    "del": delete *.log files
+    "help": Greetings! again!
+    "exit": exit
+'''
+
+INPUT_ERR_MSG = 'Invalid Input! more info -- type "help" or "h"'
+
+def exec_win32_(command: str) -> Union[None, bool]:
+    command = command.split()
+    if command[0] == 'mt':
+        if len(command) == 1:
+            subprocess.Popen(['start', 'python', multithread_server_path], shell=True)
+            time.sleep(2)
+            subprocess.Popen(['start', 'python', multithread_test_path], shell=True)
+        elif len(command) == 2:
+            if command[1] == 't':
+                subprocess.Popen(['start', 'python', multithread_test_path], shell=True)
+            elif command[1] == 's':
+                subprocess.Popen(['start', 'python', multithread_server_path], shell=True)
+            else:
+                return False
+        else:
+            return False
+    elif command[0] == 'del':
+        subprocess.Popen(['del', '*.log'], shell=True)
+    elif command[0] == 'mc':
+        subprocess.Popen(['start', 'python', multicast_test_path], shell=True)
+    else:
+        return False
+
+
+def exec_linux_(command: str) -> Union[None, bool]:
+    command = command.split()
+    print(command[0])
+    if command[0] == 'mt':
+        if len(command) == 1:
+            cmd = f'gnome-terminal -- python3 {multithread_server_path}'
+            subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+            time.sleep(2)
+            cmd = f'gnome-terminal -- python3 {multithread_test_path}'
+            subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        elif len(command) == 2:
+            if command[1] == 't':
+                cmd = f'gnome-terminal -- python3 {multithread_test_path}'
+                subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+            elif command[1] == 's':
+                cmd = f'gnome-terminal -- python3 {multithread_server_path}'
+                subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+            else:
+                return False
+        else:
+            return False
+    elif command[0] == 'del':
+        for f in glob('*.log'): 
+            os.remove(f)
+    elif command[0] == 'mc':
+        if len(command) == 1:
+            cmd = f'gnome-terminal -- python3 {multicast_test_path}'
+            subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    else:
+        return False
 
 if __name__ == '__main__':
-    
-    if sys.platform == 'win32':
-        if len(sys.argv) > 1:
-            if sys.argv[1] == 'del':
-                subprocess.Popen(['del', '*.log'], shell=True)
-            elif sys.argv[1] == 'test':
-                subprocess.Popen(['start', 'python', r'.\src\test.py'], shell=True)
-            elif sys.argv[1] == 'server':
-                subprocess.Popen(['start', 'python', r'.\src\test.py'], shell=True)
-            elif sys.argv[1] == 'mul':
-                subprocess.Popen(['start', 'python', r'.\multicast\multicast.py'], shell=True)
-        else:
-            subprocess.Popen(['start', 'python', r'.\src\server.py'], shell=True)
-            time.sleep(2)
-            subprocess.Popen(['start', 'python', r'.\src\test.py'], shell=True)
-    
-    elif sys.platform == 'linux': # Ubuntu
-        if len(sys.argv) > 1:
-            if sys.argv[1] == 'del':
-                for f in glob('*.log'): 
-                    os.remove(f)
-            elif sys.argv[1] == 'test':
-                cmd = 'gnome-terminal -- python3 ./src/test.py'
-                subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-            elif sys.argv[1] == 'server':
-                cmd = 'gnome-terminal -- python3 ./src/server.py'
-                subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-            elif sys.argv[1] == 'mul':
-                cmd = 'gnome-terminal -- python3 ./multicast/multicast.py'
-                subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-
-        else:
-            cmd = 'gnome-terminal -- python3 ./src/server.py'
-            subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-            time.sleep(2)
-            cmd = 'gnome-terminal -- python3 ./src/test.py'
-            subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    print(GREETINGS)
+    line_i = 1
+    while True:
+        try:
+            command = str(input(f'[{line_i}]: '))
+            if command in ('exit', 'e'):
+                break
+            if command in ('help', 'h'):
+                print(GREETINGS)
+                continue
+            if sys.platform == 'win32':
+                if exec_win32_(command) is False:
+                    print(INPUT_ERR_MSG)
+            elif sys.platform == 'linux': # Ubuntu
+                if exec_linux_(command) is False:
+                    print(INPUT_ERR_MSG)
+            else:
+                print('Not using Linux or Windows? \n BYE!')
+                break
+        except Exception as e:
+            print(f'Exception: {e}')
+            continue
+        finally:
+            line_i += 1
